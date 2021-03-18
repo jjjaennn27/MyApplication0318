6e2ce473ca39ca73d2cce5342b677760058bd91d
jea7982@naver.com <jea7982@naver.com>
2021-03-18 13:23:22 +0900

    0318

--- app/src/main/java/com/example/myapplication0318/SandBox.java
+++ app/src/main/java/com/example/myapplication0318/SandBox.java
@@ -1,5 +1,9 @@
 package com.example.myapplication0318;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SandBox {
+    private List<String> mGreetings = new ArrayList<>();
 
 }
